0a9ec006c36c2919088b9f21eab460270b46dfb2
Adham Mohamed <adhamdr10@gmail.com>
2025-08-17 17:29:03 +0300

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Product_Assiment.Models;
+
+namespace Product_Assiment.Controllers
+{
+    public class ProductController : Controller
+    {
+        //  /Product/ShowAll
+        public IActionResult ShowAll()
+        {
+            ProductBL productBL = new ProductBL();
+            List<Product> productsListModel = productBL.GetAll();
+            return View("ShowAll", productsListModel);
+        }
+
+        //  /Product/ShowProduct?id=1
+        public IActionResult ShowProduct(int id)
+        {
+            ProductBL productBL = new ProductBL();
+            Product productModel = productBL.GetProductById(id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+            return View("ShowProduct", productModel);
+        }
+    }
+}
---
+++ Models/ProductBL.cs
@@ -0,0 +1,31 @@
+﻿namespace Product_Assiment.Models
+{
+    public class ProductBL
+    {
+        List<Product> products;
+
+        public ProductBL()
+        {
+            products = new List<Product>
+            {
+                new Product { Id = 1, Name = "S25 Ultra", Price = 500m, ImageUrl = "S25 Ultra.png" },
+                new Product { Id = 2, Name = "A72", Price = 350m, ImageUrl = "A72.png" },
+                new Product { Id = 3, Name = "A56", Price = 400m, ImageUrl = "A56.png" },
+                new Product { Id = 4, Name = "A23", Price = 250m, ImageUrl = "A23.jpg" },
+                new Product { Id = 5, Name = "A15", Price = 200m, ImageUrl = "A15.png" },
+                new Product { Id = 6, Name = "A06", Price = 150m, ImageUrl = "A06.png" },
+            };
+        }
+
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+
+        public Product GetProductById(int id)
+        {
+            return products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+
+}
